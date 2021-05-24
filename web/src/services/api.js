52219e72402ac15fb2961f47cf5970328137ad4f
Daniel Marques <daniel.brz2009@gmail.com>
2021-05-24 19:26:41 -0300

    services

---
+++ web/src/services/api.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-param-reassign */
+import axios from 'axios';
+import { getToken } from './auth';
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3333',
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = getToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
