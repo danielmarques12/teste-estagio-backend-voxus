0437504076338b8fffa7c2d3562a5f7f32e1e84b
Daniel Marques <daniel.brz2009@gmail.com>
2021-05-24 23:51:49 -0300

    Post Page

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import Home from './pages/Home';
+// import Post from './pages/Post';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        {/* <Route path="/post/:id" component={Post} /> */}
       </Switch>
     </BrowserRouter>
   );
